9b82fba33ac7ba7b5cf127891af58b7faaae4e85
Adam Elsodaney <adam.elso@gmail.com>
2018-10-31 00:41:06 +0000

    Build with Heroku (7): Import Foundation Sass from Node Modules directory.

--- config/compass.rb
+++ config/compass.rb
@@ -1,5 +1,5 @@
 # Require any additional compass plugins here.
-add_import_path "web/components/foundation/scss"
+add_import_path "node_modules/@bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
